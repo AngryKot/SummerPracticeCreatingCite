82bd4c683810d6937b31938ec05dd5dab87af946
AngryKot <112689306+AngryKot@users.noreply.github.com>
2023-06-20 21:58:11 +0300

    Add files via upload

---
+++ liquid/src/test/java/com/Bottle/liquid/LiquidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bottle.liquid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
