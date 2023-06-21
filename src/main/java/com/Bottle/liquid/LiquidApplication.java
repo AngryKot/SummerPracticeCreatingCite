4a65102da018c5065f78f6928acddfa9fc77d41d
AngryKot <segeda.nikita2016@yandex.ru>
2023-06-21 15:11:22 +0300

    Update formating

---
+++ src/main/java/com/Bottle/liquid/LiquidApplication.java
@@ -0,0 +1,14 @@
+package com.Bottle.liquid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidApplication.class, args);
+	}
+
+
+}
